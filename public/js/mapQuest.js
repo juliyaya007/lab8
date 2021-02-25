33fff33ccac20045244613887eed70b7ee5b874b
Julia Xu <jlxu@ucsd.edu>
2021-02-25 10:52:16 -0800

    lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key='9GVpSVWPKB8LqmAyYQcGlcWgGeacgkbO';
+
+	//'map' refers to a <div> eleemnt with the ID map
+	var map = L.mapquest.map('map',{
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom:20,
+		zoomControl: false
+	});
+
+	//add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
